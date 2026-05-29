57f1ad89903f4b6f04a3c7f45ee8b6a1d58a6a02
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-05-29 20:00:17 -0300

    Reorganizando o  arquio na pasta Predictions

---
+++ backend/Vanguard/Vanguard.Application/DTOs/Predictions/PredictionRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vanguard.Application.DTOs.Predictions
+{
+    public class PredictionRequestDto
+    {
+        public string Question { get; set; } = string.Empty;
+    }
+}
