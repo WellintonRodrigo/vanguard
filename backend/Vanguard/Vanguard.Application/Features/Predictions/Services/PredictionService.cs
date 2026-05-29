12d8a786899583e43ce8675b6566843235eb2997
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-05-29 20:05:40 -0300

    Alinhando using

--- backend/Vanguard/Vanguard.Application/Features/Predictions/Services/PredictionService.cs
+++ backend/Vanguard/Vanguard.Application/Features/Predictions/Services/PredictionService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Vanguard.Application.DTOs;
+using Vanguard.Application.DTOs.Predictions;
 
 namespace Vanguard.Application.Features.Predictions.Services
 {
