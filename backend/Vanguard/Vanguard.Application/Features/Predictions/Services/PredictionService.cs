999ccb69dbc153ed24e05bedaf85cb5dc8ba9794
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-05-29 15:33:36 -0300

    Feat: Criando PredictionServices

---
+++ backend/Vanguard/Vanguard.Application/Features/Predictions/Services/PredictionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vanguard.Application.DTOs;
+
+namespace Vanguard.Application.Features.Predictions.Services
+{
+    public class PredictionService
+    {
+        public PredictionResponseDto Analyze (string question)
+        {
+            return new PredictionResponseDto
+            {
+                Probability = 0.75,
+                Confidence = 0.85,
+                Trend = "Upward",
+                AnalyzedAt = DateTime.UtcNow
+            };
+        }
+    }
+}
