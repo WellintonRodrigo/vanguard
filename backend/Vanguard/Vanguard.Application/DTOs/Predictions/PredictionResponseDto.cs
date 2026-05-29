e3ca92aa564550956e86c8f5e432af891c1554ca
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-05-29 20:01:59 -0300

    Reorganizando o  arquio na pasta Predictions e adicionando a var Product

---
+++ backend/Vanguard/Vanguard.Application/DTOs/Predictions/PredictionResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vanguard.Application.DTOs.Predictions
+{
+    public class PredictionResponseDto
+    {
+        public string Product { get; set; } = string.Empty;
+        public double Probability { get; set; }
+
+        public double Confidence { get; set; }
+
+        public string Trend { get; set; } = string.Empty;
+
+        public DateTime AnalyzedAt { get; set; }
+    }
+}
